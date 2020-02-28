eda713d01428a8c027d8edc4171b4b24d2b122f5
arun199941 <37949912+arun199941@users.noreply.github.com>
2020-02-28 14:33:19 +0530

    Update ConsistentFinder.py

--- ConsistentFinder.py
+++ ConsistentFinder.py
@@ -9,14 +9,17 @@ c1 = int(input("Enter c1 value of the polynomial: "))
 c2 = int(input("Enter c2 value of the polynomial: "))
 
 if a1/a2 == b1/b2 == c1/c2:
+    print("Consistent")
     print("Infinite Solution ")
     print("The Line Will Coincide Each Other")
    
 elif a1/a2 != b1/b2:
+    print("Consistent")
     print("Unique Solution ")
     print("The Line will Intersect at One Point")
 
 elif b1/b2 != c1/c2 or a1/a2 != c1/c2 :
+    print("InConsistent")
     print("No Solutions ")
     print("The Line Will be Paralel")
 
